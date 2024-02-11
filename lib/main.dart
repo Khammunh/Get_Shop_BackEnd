300a2439c13e0e74374e709e27e4df4294aeb2cf
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-02-11 20:47:41 +0700

    textformfield

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:getxshop/users/authentication/login_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
--- lib/users/authentication/login_screen.dart
+++ lib/users/authentication/login_screen.dart
@@ -8,12 +8,92 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var _formKey = GlobalKey<FormState>();
+  var _emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Screen'),
-      ),
+      backgroundColor: Colors.blue,
+      body: LayoutBuilder(builder: (context, cons) {
+        return ConstrainedBox(
+          constraints: BoxConstraints(minHeight: cons.maxHeight),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Login screen header
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 285,
+                  child: Image.asset(
+                    "assets/images/logo_clothes.jpg",
+                  ),
+                ),
+                //Login screen sign-in form
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(60),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black,
+                        offset: Offset(0, -3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            //email
+                            TextFormField(
+                              controller: _emailController,
+                              validator: (val) =>
+                                  val == "" ? "Please write email" : null,
+                              decoration: InputDecoration(
+                                  prefixIcon: const Icon(
+                                    Icons.email,
+                                    color: Colors.black,
+                                  ),
+                                  hintText: 'email',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide:
+                                        const BorderSide(color: Colors.white),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide:
+                                        const BorderSide(color: Colors.white),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide:
+                                        const BorderSide(color: Colors.white),
+                                  ),
+                                  disabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide:
+                                        const BorderSide(color: Colors.white),
+                                  ),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 14, vertical: 6)),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
